7d6955961f0f8f427494c707524869f9335e46e0
Zay Smith <95258445+PeaceZay@users.noreply.github.com>
2023-02-17 06:25:05 -0600

    Deployment

---
+++ src/backend/sql/tables.sql
@@ -0,0 +1,7 @@
+-- create table users (
+--     id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     email VARCHAR(50) not null UNIQUE,
+--     pw_hash VARCHAR(1000) NOT NULL
+-- );
+
+-- insert into users(email, pw_hash) values (?, ?)
